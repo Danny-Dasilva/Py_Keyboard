ccc2f7a011c54ce1b69715f990e7ecff61acb980
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-09-13 18:57:19 -0400

    init

---
+++ test.py
@@ -0,0 +1,26 @@
+class Press():
+  def __init__(self, string):
+    self.string = string
+  def test(string):
+    #!/usr/bin/env python3
+    NULL_CHAR = chr(0)
+
+    def write_report(report):
+        with open('/dev/hidg0', 'rb+') as fd:
+            fd.write(report.encode())
+
+    for c in string:
+      print(c)
+      if c == 'a':
+        print("a")
+        write_report(NULL_CHAR*2+chr(4)+NULL_CHAR*5)
+    
+
+class type:
+  def __init__(self, string):
+    self.string = string
+  
+
+
+Press.test('Hallo Test')
+
