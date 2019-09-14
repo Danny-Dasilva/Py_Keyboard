23935e1718176a9c76673b216a31f8996968125b
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-09-13 21:18:52 -0400

    function within class

--- test.py
+++ test.py
@@ -98,8 +98,8 @@ class Write():
     NULL_CHAR = chr(0)
     
     print(string)
-    self.write_report(NULL_CHAR*2+chr(40)+NULL_CHAR*5)
-    self.write_report(NULL_CHAR*8)
+    Write.write_report(NULL_CHAR*2+chr(40)+NULL_CHAR*5)
+    Write.write_report(NULL_CHAR*8)
 
 
 
