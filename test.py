c988f74461620d43dddf7949cf2352a8bcb5c760
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-09-13 21:04:19 -0400

    test class

--- test.py
+++ test.py
@@ -20,46 +20,81 @@ class Write():
       if ord(c) > 96:
           s = ord(c) - 93
           write_report(NULL_CHAR*2+chr(s)+NULL_CHAR*5)
-          write_report(NULL_CHAR*8)
       #uppercasse
       if 64 < ord(c) < 91:
           a = ord(c) - 61
-          write_report(chr(32)+NULL_CHAR+chr(a)+NULL_CHAR*5)
-          write_report(NULL_CHAR*8)
-          
+          write_report(chr(32)+NULL_CHAR+chr(a)+NULL_CHAR*5) 
       #numbers
       if 48 < ord(c) < 58:
           a = ord(c) - 19
           write_report(NULL_CHAR*2+chr(a)+NULL_CHAR*5)
-          write_report(NULL_CHAR*8)
       # 0 
       if ord(c) == 48:
           a = ord(c) - 9
           write_report(NULL_CHAR*2+chr(a)+NULL_CHAR*5)
-          write_report(NULL_CHAR*8)
+     
+      if c == '!':
+        write_report(chr(32)+NULL_CHAR+chr(30)+NULL_CHAR*5)
+     
+      if c == '@':
+        write_report(chr(32)+NULL_CHAR+chr(31)+NULL_CHAR*5)
+     
+      if c == '#':
+        write_report(chr(32)+NULL_CHAR+chr(32)+NULL_CHAR*5)
+   
+      if c == '$':
+        write_report(chr(32)+NULL_CHAR+chr(33)+NULL_CHAR*5)
+  
+      if c == '%':
+        write_report(chr(32)+NULL_CHAR+chr(34)+NULL_CHAR*5)
+  
+      if c == '^':
+        write_report(chr(32)+NULL_CHAR+chr(35)+NULL_CHAR*5)
+       
+      if c == '&':
+        write_report(chr(32)+NULL_CHAR+chr(36)+NULL_CHAR*5)
+      
+      if c == '*':
+        write_report(chr(32)+NULL_CHAR+chr(37)+NULL_CHAR*5)
+       
+      if c == '(':
+        write_report(chr(32)+NULL_CHAR+chr(38)+NULL_CHAR*5)
+      if c == ')':
+        write_report(chr(32)+NULL_CHAR+chr(39)+NULL_CHAR*5)
+      if c == '=':
+        write_report(NULL_CHAR*2+chr(46)+NULL_CHAR*5)
+      if c == '[':
+        write_report(chr(32)+NULL_CHAR+chr(47)+NULL_CHAR*5)
+      if c == '{':
+        write_report(NULL_CHAR*2+chr(47)+NULL_CHAR*5)
+      if c == ']':
+        write_report(chr(32)+NULL_CHAR+chr(48)+NULL_CHAR*5)
+      if c == '}':
+        write_report(NULL_CHAR*2+chr(48)+NULL_CHAR*5)
+      # if c == "\":
+      #   write_report(chr(32)+NULL_CHAR+chr(47)+NULL_CHAR*5)
+      if c == '|':
+        write_report(NULL_CHAR*2+chr(49)+NULL_CHAR*5)
+      if c == ';':
+        write_report(chr(32)+NULL_CHAR+chr(51)+NULL_CHAR*5)
+      if c == ':':
+        write_report(NULL_CHAR*2+chr(51)+NULL_CHAR*5)
+     
+      if c == "'":
+        write_report(NULL_CHAR*2+chr(52)+NULL_CHAR*5)
+
+      if c == '"':
+        write_report(chr(32)+NULL_CHAR+chr(52)+NULL_CHAR*5)
+     
+
+
+      write_report(NULL_CHAR*8)
 
 
 
-# ! 30
-# @ 31
-# 32 #
-# 33 $
-# 34%
-# 23 ^
-# 36 &
-# 37 *
-# 38 (
-# 39 )
-# 46 = +
 
 
-# 47 [{
-# 48 ]}
-# 49 \|
-
-# 51 ;:
-# 52 '"
-
+# to do
 # 54 , <
 # 55 . >
 # 56  / ?
@@ -72,7 +107,7 @@ class type:
   
 
 
-Write.write('012 3456 789A BCDabcd zzZZ')
+Write.write('!@#$%^&*()[]{}=123qwe')
 
 
 
