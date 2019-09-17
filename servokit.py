f04465a752263bdfc75d1433aa1cd07cf8a00d04
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-09-17 03:32:29 -0400

    test for commands

--- servokit.py
+++ servokit.py
@@ -1,6 +1,7 @@
 
 from app.ada_hid import Keyboard
 from app.ada_hid import KeyboardLayoutUS
+from app.keycode import Keycode
 import time
 time.sleep(2)
 kbd = Keyboard()
@@ -8,6 +9,8 @@ layout = KeyboardLayoutUS(kbd)
 
 # Type 'abc' followed by Enter (a newline).
 layout.write('hello is this working\n')
+kbd.press(Keycode.CONTROL, Keycode.ALT, Keycode.T )
+kbd.release_all()
 
 # Get the keycodes needed to type a '$'.
 # The method will return (Keycode.SHIFT, Keycode.FOUR).
