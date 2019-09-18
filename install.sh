1881d51e5774e047f9df246caa3b15272094c507
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-09-18 01:46:14 -0400

    sudo run file

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "dwc2" | sudo tee -a /etc/modules
 sudo echo "libcomposite" | sudo tee -a /etc/modules
 sudo touch /usr/bin/isticktoit_usb
 sudo chmod +x /usr/bin/isticktoit_usb
-line="@reboot /usr/bin/isticktoit_usb # libcomposite configuration"
+line="@reboot sudo /usr/bin/isticktoit_usb"
     (crontab -u "pi" -l; echo "$line" ) | crontab -u "pi" -
 
-cp usb.sh /usr/bin/isticktoit_usb
+sudo cp usb.sh /usr/bin/isticktoit_usb
